f4589f0888fa32c19e85e3c52f56aae0d7cbf4f4
Xsy <1006937132@qq.com>
2023-01-31 10:03:22 +0800

    底部导航栏实现

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import routes from "@/router";
+import NavBar from "@/components/NavBar";
 
 import { ConfigProvider } from "zarm";
 import zhCN from "zarm/lib/config-provider/locale/zh_CN";
 import "zarm/dist/zarm.css";
 
 function App() {
+  const location = useLocation(); // 拿到 location 实例
+  const { pathname } = location; // 获取当前路径
+  const needNav = ["/", "/data", "/user"]; // 需要底部导航栏的路径
+  const [showNav, setShowNav] = useState(false); // 是否展示NavBar
+  useEffect(() => {
+    setShowNav(needNav.includes(pathname));
+  }, [pathname]); // [] 内的参数若是变化，便会执行上述回调函数=
+
   return (
-    <Router>
-      <ConfigProvider primaryColor={"#007fff"} locale={zhCN}>
-        <>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                exact
-                key={route.path}
-                path={route.path}
-                element={<route.component />}
-              />
-            ))}
-          </Routes>
-        </>
-      </ConfigProvider>
-    </Router>
+    <ConfigProvider primaryColor={"#007fff"}>
+      <>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              exact
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+        <NavBar showNav={showNav} />
+      </>
+    </ConfigProvider>
   );
 }
 
