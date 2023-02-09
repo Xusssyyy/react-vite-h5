cc10c73c9bbbc932485950354414b557a64c5290
Xsy <1006937132@qq.com>
2023-02-09 09:29:29 +0800

    账单列表页

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,30 +1,45 @@
 // Home/index.jsx
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Icon, Pull } from "zarm";
 import s from "./style.module.less";
 import dayjs from "dayjs";
 import BillItem from "@/components/BillItem";
 import { get, REFRESH_STATE, LOAD_STATE } from "@/utils"; // Pull 组件需要的一些常量
+import PopupType from "@/components/PopupType";
+import PopupDate from "@/components/PopupDate";
+import CustomIcon from "@/components/CustomIcon";
+import PopupAddBill from "@/components/PopupAddBill";
 
 const Home = () => {
+  const monthRef = useRef(); // 月份筛选 ref
   const [currentTime, setCurrentTime] = useState(dayjs().format("YYYY-MM")); // 当前筛选时间
   const [page, setPage] = useState(1); // 分页
   const [list, setList] = useState([]); // 账单列表
   const [totalPage, setTotalPage] = useState(0); // 分页总数
   const [refreshing, setRefreshing] = useState(REFRESH_STATE.normal); // 下拉刷新状态
   const [loading, setLoading] = useState(LOAD_STATE.normal); // 上拉加载状态
+  const typeRef = useRef(); // 账单类型ref
+  const [currentSelect, setCurrentSelect] = useState({}); //当前筛选类型
+  const [totalExpense, setTotalExpense] = useState(0); // 总支出
+  const [totalIncome, setTotalIncome] = useState(0); // 总收入
+  const addRef = useRef(); // 添加账单 ref
 
   // 获取账单方法
   const getBillList = async () => {
     const { data } = await get(
-      `/api/bill/list?page=${page}&page_size=5&date=${currentTime}`
+      `/api/bill/list?page=${page}&page_size=5&date=${currentTime}&type_id=${
+        currentSelect.id || "all"
+      }`
     );
+    console.log(data, "/////");
     // 下拉刷新，重制数据
     if (page == 1) {
       setList(data.list);
     } else {
       setList(list.concat(data.list));
     }
+    setTotalExpense(data.totalExpense.toFixed(2));
+    setTotalIncome(data.totalIncome.toFixed(2));
     setTotalPage(data.totalPage);
     // 上滑加载状态
     setLoading(LOAD_STATE.success);
@@ -48,40 +63,67 @@ const Home = () => {
     }
   };
 
+  // 添加账单弹窗
+  const toggle = () => {
+    typeRef.current && typeRef.current.show();
+  };
+
+  // 筛选类型
+  const select = (item) => {
+    setRefreshing(REFRESH_STATE.loading);
+    // 触发刷新列表，将分页重制为 1
+    setPage(1);
+    setCurrentSelect(item);
+  };
+
+  // 选择月份弹窗
+  const monthToggle = () => {
+    monthRef.current && monthRef.current.show();
+  };
+
+  // 筛选月份
+  const selectMonth = (item) => {
+    setRefreshing(REFRESH_STATE.loading);
+    setPage(1);
+    setCurrentTime(item);
+  };
+
+  // 添加账单弹窗
+  const addToggle = () => {
+    addRef.current && addRef.current.show();
+  };
+
   useEffect(() => {
     getBillList(); //初始化
-  }, [page]);
+  }, [page, currentSelect, currentTime]);
 
   return (
     <div className={s.home}>
       <div className={s.header}>
         <div className={s.dataWrap}>
           <span className={s.expense}>
-            总支出：<b>¥ 200000</b>
+            总支出：<b>¥ {totalExpense}</b>
           </span>
           <span className={s.income}>
-            总收入：<b>¥ 500000</b>
+            总收入：<b>¥ {totalIncome}</b>
           </span>
         </div>
         <div className={s.typeWrap}>
-          <div className={s.left}>
+          <div className={s.left} onClick={toggle}>
             <span className={s.title}>
-              类型 <Icon className={s.arrow} type="arrow-bottom" />
+              {currentSelect.name || "全部类型"}{" "}
+              <Icon className={s.arrow} type="arrow-bottom" />
             </span>
           </div>
           <div className={s.right}>
-            <span className={s.time}>
-              2022-06
+            <span className={s.time} onClick={monthToggle}>
+              {currentTime}
               <Icon className={s.arrow} type="arrow-bottom" />
             </span>
           </div>
         </div>
       </div>
-
       <div className={s.contentWrap}>
-        {/* {list.map((item, index) => (
-          <BillItem bill={item} key={index} />
-        ))} */}
         {list.length ? (
           <Pull
             animationDuration={200}
@@ -102,6 +144,12 @@ const Home = () => {
           </Pull>
         ) : null}
       </div>
+      <PopupType ref={typeRef} onSelect={select} />
+      <PopupDate ref={monthRef} mode="month" onSelect={selectMonth} />
+      <div className={s.add} onClick={addToggle}>
+        <CustomIcon type="tianjia" />
+      </div>
+      <PopupAddBill ref={addRef} onReload={refreshData} />
     </div>
   );
 };
