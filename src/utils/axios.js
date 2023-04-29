df6c00cedf0de79949c6dd1deea073f95b72ae6b
Xsy <1006937132@qq.com>
2023-04-29 15:04:36 +0800

    modify

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -7,12 +7,12 @@ const MODE = import.meta.env.MODE; // 环境变量
 axios.defaults.baseURL =
   MODE == "development" ? "http://127.0.0.1:7001" : "http://127.0.0.1:7001"; // koa-egg插件处理跨域
 // MODE == "development" ? "/api" : "/api"; // 配置代理处理跨域
-axios.defaults.withCredentials = true;
-axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
+axios.defaults.withCredentials = true; // 将withCredentials选项设置为true，这允许Axios随跨域请求一起发送cookie。
+axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest"; // 将X-Requested-With头设置为XMLHttpRequest，这是一个常用的头，用于标识AJAX请求。
 axios.defaults.headers["Authorization"] = `${
   localStorage.getItem("token") || null
-}`;
-axios.defaults.headers.post["Content-Type"] = "application/json";
+}`; // 将Authorization头设置为浏览器本地存储中token项的值。这允许客户端使用基于令牌的身份验证系统进行身份验证。
+axios.defaults.headers.post["Content-Type"] = "application/json"; // 将Content-Type头设置为application/json，用于POST请求，告诉服务器请求体将以JSON格式发送。
 
 axios.interceptors.response.use((res) => {
   if (typeof res.data !== "object") {
