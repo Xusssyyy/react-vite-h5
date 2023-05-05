7f681b2e5a2ee89ed9ecdb91195131579f8ccd81
Xsy <1006937132@qq.com>
2023-05-05 23:16:46 +0800

    关于我们

---
+++ src/container/About/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '@/components/Header'
+
+const About = () => {
+  return <>
+    <Header title='关于我们' />
+    <div>
+        这是一个记账本项目
+    </div>
+  </>
+};
+
+export default About;
